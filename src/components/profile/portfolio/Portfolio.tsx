48c605c302a420b817d894ea17b7f2092b39ef47
Ishant Chauhan <ishantchauhan710@gmail.com>
2023-06-25 13:30:36 +0530

    Projects section added

--- src/components/profile/portfolio/Portfolio.tsx
+++ src/components/profile/portfolio/Portfolio.tsx
@@ -25,8 +25,44 @@ const Portfolio = () => {
     },
   ];
 
+  const projects = [
+    {
+      title: "Personal Portfolio Website",
+      image: "/project_img.png",
+      skills: [
+        { name: "HTML/CSS", logo: "/html5.png" },
+        { name: "React", logo: "/react.png" },
+      ],
+    },
+    {
+      title: "Personal Portfolio Website",
+      image: "/project_img.png",
+      skills: [
+        { name: "HTML/CSS", logo: "/html5.png" },
+        { name: "React", logo: "/react.png" },
+      ],
+    },
+    {
+      title: "Personal Portfolio Website",
+      image: "/project_img.png",
+      skills: [
+        { name: "HTML/CSS", logo: "/html5.png" },
+        { name: "React", logo: "/react.png" },
+      ],
+    },
+    {
+      title: "Personal Portfolio Website",
+      image: "/project_img.png",
+      skills: [
+        { name: "HTML/CSS", logo: "/html5.png" },
+        { name: "React", logo: "/react.png" },
+      ],
+    },
+  ];
+
   return (
     <div>
+      {/* STATS */}
       <div className="portfolio-section">
         <div className="portfolio-label">Stats</div>
         <div className="grid grid-cols-2 gap-4">
@@ -41,6 +77,50 @@ const Portfolio = () => {
                   {stat.value}
                 </div>
                 <div className="text-neutral-700 text-sm">{stat.label}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* PROJECTS */}
+      <div className="portfolio-section">
+        <div className="flex items-center justify-between">
+          <div className="portfolio-label">Projects</div>
+          <div className="portfolio-label-action">Create new project</div>
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+          {projects.map((project, i) => (
+            <div
+              key={project.title}
+              className="bg-neutral-100 text-gray-800 cursor-pointer p-4 text-sm rounded-md"
+            >
+              <Image
+                src={project.image}
+                width={1000}
+                height={1000}
+                alt={project.title}
+              />
+
+              <div className="text-neutral-900 font-semibold text-lg mt-2">
+                {project.title}
+              </div>
+              <div className="flex mt-1">
+                {project.skills.map((skill) => (
+                  <div key={skill.name} className="flex">
+                    <Image
+                      src={skill.logo}
+                      width={20}
+                      height={20}
+                      alt={skill.name}
+                    />
+                    <div className="text-sm font-light ml-2">{skill.name}</div>
+                    {skill.name !==
+                      project.skills[project.skills.length - 1].name && (
+                      <div className="mx-3">•</div>
+                    )}
+                  </div>
+                ))}
               </div>
             </div>
           ))}
