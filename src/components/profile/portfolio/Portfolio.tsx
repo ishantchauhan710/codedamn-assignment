431587c5926d496d9b5f751190b46da287c0ff85
Ishant Chauhan <ishantchauhan710@gmail.com>
2023-06-25 14:01:01 +0530

    Playground section completed

--- src/components/profile/portfolio/Portfolio.tsx
+++ src/components/profile/portfolio/Portfolio.tsx
@@ -60,6 +60,65 @@ const Portfolio = () => {
     },
   ];
 
+  const playgrounds = [
+    {
+      title: "Playground title",
+      language: "HTML/CSS",
+      time: "1min ago",
+      logo: "/html5big.png",
+      sharedWith: [
+        { name: "Adam", pic: "/dummyuser1.png" },
+        { name: "Anna", pic: "/dummyuser2.png" },
+        { name: "Ishant", pic: "/dummyuser1.png" },
+        { name: "Adam", pic: "/dummyuser2.png" },
+        { name: "Anna", pic: "/dummyuser1.png" },
+        { name: "Ishant", pic: "/dummyuser2.png" },
+      ],
+    },
+    {
+      title: "Playground title",
+      language: "HTML/CSS",
+      time: "1min ago",
+      logo: "/html5big.png",
+      sharedWith: [
+        { name: "Adam", pic: "/dummyuser1.png" },
+        { name: "Anna", pic: "/dummyuser2.png" },
+        { name: "Ishant", pic: "/dummyuser1.png" },
+        { name: "Adam", pic: "/dummyuser2.png" },
+        { name: "Anna", pic: "/dummyuser1.png" },
+        { name: "Ishant", pic: "/dummyuser2.png" },
+      ],
+    },
+    {
+      title: "Playground title",
+      language: "HTML/CSS",
+      time: "1min ago",
+      logo: "/html5big.png",
+      sharedWith: [
+        { name: "Adam", pic: "/dummyuser1.png" },
+        { name: "Anna", pic: "/dummyuser2.png" },
+        { name: "Ishant", pic: "/dummyuser1.png" },
+        { name: "Adam", pic: "/dummyuser2.png" },
+        { name: "Anna", pic: "/dummyuser1.png" },
+        { name: "Ishant", pic: "/dummyuser2.png" },
+      ],
+    },
+    {
+      title: "Playground title",
+      language: "HTML/CSS",
+      time: "1min ago",
+      logo: "/html5big.png",
+      sharedWith: [
+        { name: "Adam", pic: "/dummyuser1.png" },
+        { name: "Anna", pic: "/dummyuser2.png" },
+        { name: "Ishant", pic: "/dummyuser1.png" },
+        { name: "Adam", pic: "/dummyuser2.png" },
+        { name: "Anna", pic: "/dummyuser1.png" },
+        { name: "Ishant", pic: "/dummyuser2.png" },
+      ],
+    },
+  ];
+
   return (
     <div>
       {/* STATS */}
@@ -121,6 +180,67 @@ const Portfolio = () => {
                     )}
                   </div>
                 ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* PLAYGROUNDS */}
+      <div className="portfolio-section">
+        <div className="flex items-center justify-between">
+          <div className="portfolio-label">Playgrounds</div>
+          <div className="portfolio-label-action">Create new playground</div>
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+          {playgrounds.map((playground) => (
+            <div
+              key={playground.title}
+              className="bg-neutral-200/40  text-gray-800 cursor-pointer p-2 text-sm rounded-md mr-2 flex items-start"
+            >
+              <Image
+                src={playground.logo}
+                width={40}
+                height={40}
+                alt={playground.title}
+                className="m-2"
+              />
+              <div className="flex flex-col ml-2">
+                <div className="text-neutral-700 font-semibold text-lg">
+                  {playground.title}
+                </div>
+                <div className="text-neutral-700 text-sm font-light mt-1">
+                  {playground.language} • {playground.time}
+                </div>
+                <div className="mt-2 flex items-center">
+                  {playground.sharedWith.slice(0, 2).map((user) => (
+                    <div
+                      key={user.name}
+                      className="border-1 border-white w-6 h-6 rounded-full overflow-hidden ml-[-6px]"
+                    >
+                      <Image
+                        src={user.pic}
+                        width={40}
+                        height={40}
+                        alt={user.name}
+                      />
+                    </div>
+                  ))}
+                  <div className="ml-1 text-sm text-neutral-600">
+                    Shared with{" "}
+                    {playground.sharedWith.slice(0, 2).map((user) => (
+                      <span className="font-bold" key={user.name + "name"}>
+                        {user.name}
+                        {user.name !== playground.sharedWith[1].name && ", "}
+                      </span>
+                    ))}
+                    {playground.sharedWith.length > 1 && (
+                      <span className="text-sm">{`.. + ${
+                        playground.sharedWith.length - 2
+                      } others`}</span>
+                    )}
+                  </div>
+                </div>
               </div>
             </div>
           ))}
